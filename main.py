bdaa0ef07dded486e17d9d85c2e5623834bcb514
adnusan <ad.nusan@gmail.com>
2023-02-08 22:50:12 -0800

    TODO: fix pynacl error

---
+++ main.py
@@ -0,0 +1,35 @@
+
+    
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+#getting token from txt file
+with open("token.txt", "r") as f:
+    token = f.read()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        
+#join vice channel        
+@client.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is None and after.channel is not None:
+        # User just joined a voice channel
+        voice_channel = after.channel
+        print(f"{member} joined {voice_channel}")
+        await voice_channel.connect()      
+
+client.run(token)
